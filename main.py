16572990439c09ea130dce354075004056205ed3
DouglasIde <douglasymide@gmail.com>
2025-05-24 14:21:29 -0300

    Feat: Creating the switch-case loop for the user to decide what they want to see

--- main.py
+++ main.py
@@ -7,25 +7,41 @@ import matplotlib.pyplot as plt
 
 from functions import Functions
 
+def gerar_histograma():
+    print("Histrograma sendo gerado por Hora de Chegada")
+    largura_bin_at = func.calcular_largura_bin(dados, 'arrival_time')
+    sns.histplot(data=dados, x='arrival_time', kde=True, binwidth=largura_bin_at)
+    plt.show()
+    print("Histrograma sendo gerado por Hora de partida")
+    largura_bin_at = func.calcular_largura_bin(dados, 'departure_time')
+    sns.histplot(data=dados, x='departure_time', kde=True, binwidth=largura_bin_at)
+    plt.show()
+
 if __name__ == '__main__':
 
     dados = pd.read_csv('flights.csv')
     print(dados.head())
-    print(dados.info())
 
     # Chamando as funções com FUNC
     func = Functions(dados)
 
-    # Funções com Atrasos Médios
-    # func.airline_delay()
-    # func.type_average_delay()
-    # func.holiday_average_delay()
-    #
-    # # Numéro de Voos por Aeronave
-    # func.flight_numbers_by_ship()
+    print("""
+        1 - Verificar atrasos médios
+        2 - Histograma Freedman-Diaconis com Bins
+    """)
 
-    largura_bin_at = func.calcular_largura_bin(dados, 'arrival_time')
-    sns.histplot(data=dados, x='arrival_time', kde=True, binwidth=largura_bin_at)
-    plt.show()
+    switch = {
+        1: lambda: (func.airline_delay(), func.type_average_delay(), func.holiday_average_delay()),
+        2: lambda: gerar_histograma(),
+    }
+
+    user_choice = int(input("Escolha o que deseja verificar: "))
+
+    if user_choice in switch:
+        switch[user_choice]()
+    else:
+        print("Opção inválida.")
+
+
 
 
