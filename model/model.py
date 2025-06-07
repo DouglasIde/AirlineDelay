00a70325e8709591f20307a6478dd0372feebe22
DouglasIde <douglasymide@gmail.com>
2025-06-07 14:31:21 -0300

    Feat: Calling the functions in the project's main

--- model/model.py
+++ model/model.py
@@ -1,6 +1,29 @@
 from ortools.sat.python import cp_model
 
+from model.functions import avioes_distintos, todo_aviao_tem_estacionar, limita_aviao_grande_estacionamento_grande, resolve
+from model.models.aviao import Aviao
+from model.models.estacionamento import Estacionamento
+
 model = cp_model.CpModel()
+
+# Se for TRUE significa que o avião é grande, FALSE é que é pequeno
+avioes = [Aviao(1, True), Aviao(2, False), Aviao(3, True)]
+
+total_de_avioes = len(avioes)
+
+estacionamentos = [Estacionamento(1, total_de_avioes, False, model),
+                   Estacionamento(2, total_de_avioes, False, model),
+                   Estacionamento(3, total_de_avioes, True, model),
+                   Estacionamento(4, total_de_avioes, True, model)]
+
+estacionamentos[2].vizinhos = [estacionamentos[3]]
+
+avioes_distintos(estacionamentos, model)
+todo_aviao_tem_estacionar(total_de_avioes, estacionamentos, model)
+limita_aviao_grande_estacionamento_grande(model, estacionamentos, avioes)
+
+solucionador = cp_model.CpSolver()
+resolve(solucionador, model, estacionamentos, avioes)
 
 
 
